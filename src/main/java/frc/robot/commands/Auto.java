f755e07279936abac6f8da05d13ff24d00a77caa
Michael2121212121 <michaeldillman25@gmail.com>
2025-12-13 13:15:39 -0800

    not working auto. do not deploy to robot

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -1,8 +1,16 @@
 package frc.robot.commands;
 
-public class Auto {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivebase;
+
+public class Auto extends Command {
+    private static Drivebase drivebase = new Drivebase();
 
     public Auto() {
 
+    }
+
+    public Command auto() {
+        return this.run(() -> drivebase.defaultDrive(0, 1, 0, true));
     }
 }
